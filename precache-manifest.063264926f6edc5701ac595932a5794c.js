332cb84e992171e9c63f10e301173885162fe0bb
Chris Fredrick <fredrickce@gmail.com>
2020-05-28 15:15:50 -0400

    Updates

---
+++ precache-manifest.063264926f6edc5701ac595932a5794c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f6fc8fa2252ec2e41546cff800e371",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "903cafca9e35b2c80396",
+    "url": "/monsters-rolodex/static/css/main.cbb9a76e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903cafca9e35b2c80396",
+    "url": "/monsters-rolodex/static/js/main.30f37eff.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
